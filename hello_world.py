211e4cc91965a3fd70e114db6651b07944ee2d45
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-08-29 12:45:00 +0500

    Added Matrix Multiplication and commented sum function

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,32 @@
 def mod_func(a,b):
   return a%b
 
-def sum_func(a,b):
-  return a+b
+#def sum_func(a,b):
+#  return a+b
+
+A = [[12, 7, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+
+# take a 3x4 matrix
+B = [[5, 8, 1, 2],
+    [6, 7, 3, 0],
+    [4, 5, 9, 1]]
+
+# result will be 3x4
+result = [[sum(a * b for a, b in zip(A_row, B_col)) 
+                        for B_col in zip(*B)]
+                                for A_row in A]
+
+for r in result:
+    print(r)
 
 var = "hello world"
 
 a=5
 b=15
-sum = sum_func(a,b)
+#sum = sum_func(a,b)
 mod = mod_func(b,a)
 
-print(sum)
+#print(sum)
 print(mod)
