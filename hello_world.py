6bdab9340c08052e8aa55950898fa2d4244902b0
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-09-02 17:07:30 +0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,49 @@ def mod_func(a,b):
   return a%b
 
 
+
+
+def bubble_sort(arr):
+    # Outer loop to iterate through the list n times
+    for n in range(len(arr) - 1, 0, -1):
+
+        # Inner loop to compare adjacent elements
+        for i in range(n):
+            if arr[i] > arr[i + 1]:
+
+                # Swap elements if they are in the wrong order
+                swapped = True
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+
+# Sample list to be sorted
+arr = [39, 12, 18, 85, 72, 10, 2, 18]
+print("Unsorted list is:")
+print(arr)
+
+bubble_sort(arr)
+
+
+def insertion_sort(arr):
+
+    # Outer loop to iterate through the list n times
+    for n in range(len(arr) - 1, 0, -1):
+
+        # Inner loop to compare adjacent elements
+        for i in range(n):
+            if arr[i] > arr[i + 1]:
+
+                # Swap elements if they are in the wrong order
+                swapped = True
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+
+# Sample list to be sorted
+arr = [39, 12, 18, 85, 72, 10, 2, 18]
+print("Unsorted list is:")
+print(arr)
+
+
 var = "hello world"
 
 a=5
