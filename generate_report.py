25a43bbfafdc5e759446f416a5f54aeced3a91ca
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-09-03 11:32:37 +0500

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -14,22 +14,22 @@ def generate_report(diff_file):
         diff_content = f.read()
 
 
-prompt = f""" Use the following git diff file and generate a summary of all the changes that have occurred as if you were a PR reviewer bot. 
-Here is the file: {diff_content}
-   """
-
-
-response = client.chat.completions.create(
-    model='gpt-3.5-turbo-16k',
-    messages=[{"role": "user", "content": [{"type": "text", "text": prompt}]}],
-    max_tokens=1000,
-    temperature=0.2,
-    top_p=1.0
-)
-
-
-    content = response.choices[0].message.content
+    prompt = f""" Use the following git diff file and generate a summary of all the changes that have occurred as if you were a PR reviewer bot. 
+    Here is the file: {diff_content}
+       """
     
+    
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo-16k',
+        messages=[{"role": "user", "content": [{"type": "text", "text": prompt}]}],
+        max_tokens=1000,
+        temperature=0.2,
+        top_p=1.0
+    )
+    
+    
+    content = response.choices[0].message.content
+        
     return content
 
 if __name__ == "__main__":
