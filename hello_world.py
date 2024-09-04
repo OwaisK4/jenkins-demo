6bb2f300364b1cfd0e667f7b82ea4c3ab8c4b327
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-09-04 15:09:00 +0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,8 @@ def mod_func(a,b):
 def funny_func():
   print("Yippeee!")
 
-def sub_func(a,b):
-  c=a-b
+def mult_func(a,b):
+  c=a*b
   return c
 
 def div_func(a,b):
@@ -15,11 +15,11 @@ a=5
 b=14
 #sum = sum_func(a,b)
 mod = mod_func(b,a)
-sub = sub_func(a,b)
+mul = mult_func(a,b)
 div = div_func(a,b)
 
 print(sum)
-print(sub)
+print(mul)
 print(mod)
 
 funny_func()
