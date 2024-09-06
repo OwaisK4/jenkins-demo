a71f39104e87ed2f62be6da212d2890b40296603
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-09-06 19:48:39 +0500

    Update filter_users.py

--- config_files/filter_users.py
+++ config_files/filter_users.py
@@ -9,7 +9,7 @@ def get_pr_author(repo_name, pr_number):
     repo = g.get_repo(repo_name)
     pr = repo.get_pull(pr_number)
 
-    pr_author = pr.user.login.strip()
+    pr_author = str(pr.user.login.strip())
     return pr_author
 
 if __name__ == "__main__":
